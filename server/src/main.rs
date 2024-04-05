d5880bdf5adb762935479e4618d5817cfa80a37f
UDogg <choudharyu2003@gmail.com>
2024-04-05 17:49:56 -0400

    single threaded web server in Rust

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,48 @@
 use std::net::TcpListener;
-
-// fn handle_client(mut stream: TcpStream) {
-//     let mut buffer = [0; 1024];
-//     stream.read(&mut buffer).unwrap();
-
-//     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-// }
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let listener = 
         TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-        println!("Connection established!");
-        // handle_client(stream);
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
 
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    // read the request from the stream and store it in the buffer
+    stream.read(&mut buffer).unwrap();
+    // convert the buffer to a string
+
+    let get = b"GET / HTTP/1.1\r\n";
+    // check if the request is a GET request
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+        // if the request is a GET request, return the status line and the filename
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        // if the request is not a GET request, return the status line and the filename
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    // read the contents of the file and store it in the contents variable
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    // combine the status line and the contents of the file
+
+    stream.write(response.as_bytes()).unwrap();
+    // write the response to the stream
+    stream.flush().unwrap();
+    // flush the stream
 }
