e13f22a974310817f3a60fc5088ff2ccf62a2678
UDogg <choudharyu2003@gmail.com>
2024-04-05 17:16:02 -0400

    initialized project with cargo new --bin server

---
+++ server/src/main.rs
@@ -0,0 +1,20 @@
+use std::net::TcpListener;
+
+// fn handle_client(mut stream: TcpStream) {
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
+
+//     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+// }
+
+fn main() {
+    let listener = 
+        TcpListener::bind("127:0:0:1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+        // handle_client(stream);
+    }
+
+}
