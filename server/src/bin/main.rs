00fe0004a5ebf799c6654caa784efc55c65a0efb
UDogg <choudharyu2003@gmail.com>
2024-04-06 00:23:41 -0400

    Basic multithreaded server in Rust. Serve better html file later.

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -2,15 +2,26 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use server::ThreadPool;
+
 
 fn main() {
     let listener = 
         TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+
+
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 
 }
 
@@ -21,8 +32,13 @@ fn handle_connection(mut stream: TcpStream) {
     // convert the buffer to a string
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     // check if the request is a GET request
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+        // if the request is a GET request, return the status line and the filename
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
         // if the request is a GET request, return the status line and the filename
     } else {
